ccba33490e26d4847c84f71b643e1752ff4131a3
Darlington Ogbuefi <ogbuefidarlington@yahoo.com>
2025-09-22 11:10:57 +0100

    replaced already existing resources with data blocks and db.t3.medium with db.serverless

--- stack1/outputs.tf
+++ stack1/outputs.tf
@@ -6,18 +6,6 @@ output "db_reader_endpoint" {
   value = module.aurora_serverless.cluster_reader_endpoint
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "private_subnet_ids" {
-  value = module.vpc.private_subnets
-}
-
-output "public_subnet_ids" {
-  value = module.vpc.public_subnets
-}
-
 output "aurora_endpoint" {
   value = module.aurora_serverless.cluster_endpoint
 }
